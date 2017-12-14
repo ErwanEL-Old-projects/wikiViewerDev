7cc2435cc2587d964ef43e3dfffdd601e9c6f9bd
ErwanEL <leblois.erwan@hotmail.fr>
2017-12-14 18:26:23 -0500

    Added loader Updated asynchronous request, active class on click

--- app.js
+++ app.js
@@ -10,17 +10,13 @@ let results;
 let titles = [];
 let snippets = [];
 
-
-loader.style.display = 'none';
-
-
 const encodeUrl = () => {
     let encodedInput;
     encodedInput = encodeURIComponent(input.value);
     url = `https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&list=search&srsearch=${encodedInput}&utf8=`;
 };
 
-const userInterface = () =>  { 
+const userInterface = () =>  {
     results.forEach(result => {
         titles.push(result.title);
         snippets.push(result.snippet);
@@ -29,7 +25,7 @@ const userInterface = () =>  {
                                     <h5 class="mb-1">${result.title}</h5>
                                 </div>
                             <p class="mb-1">${result.snippet}</p>
-                            </a>`
+                            </a>`;
     });
 };
 
@@ -40,9 +36,12 @@ const request = function () {
         if(xhr.readyState === 4) {
             loader.style.display = 'none';
             data = JSON.parse(xhr.responseText);
-        }
-    }
-    xhr.open('GET', url, false);
+            results = data.query.search;
+            list.innerHTML = "";
+            userInterface();
+        };
+    };
+    xhr.open('GET', url, true);
     xhr.send();
 };
 
@@ -52,70 +51,21 @@ randomButton.addEventListener('click', () => {
 });
 
 go.addEventListener('click', () => {
+    list.innerHTML = "";
+    lis = [];
     encodeUrl();
     request();
-    results = data.query.search;
-    list.innerHTML = "";
-    userInterface();
 });
 
 input.addEventListener("keyup", function(event) {
     event.preventDefault();
     if (event.keyCode === 13) {
         go.click();
-    }
+    };
 });
-    
-// function loadAjax() {
-// document.getElementById('results').innerHTML = '';
-// openModal();
-// var xhr = false;
-// if (window.XMLHttpRequest) {
-//     xhr = new XMLHttpRequest();
-// }
-// else {
-//     xhr = new ActiveXObject("Microsoft.XMLHTTP");
-// }
-// if (xhr) {
-//     xhr.onreadystatechange = function () {
-//         if (xhr.readyState == 4 && xhr.status == 200) {
-//             closeModal();
-//             document.getElementById("results").innerHTML = xhr.responseText;
-//         }
-//     }
-//     xhr.open("GET", "load-ajax-data.php", true);
-//     xhr.send(null);
-// }
-// }
 
-
-
-
-
-
-
-// let lis = document.getElementsByClassName('list-group');
-// lis = lis[0].children;
-// lis.map(li => {
-//     console.log(li);
-// });
-
-// lis.addEventListener('mouseover', () => {
-//     lis.classList.add('active');
-// });
-// lis.addEventListener('mouseout', () => {
-//     lis.classList.remove('active');
-// });
-
-// $(function(){
-//     console.log('ready');
-
-//     $('.list-group li').click(function(e) {
-//         e.preventDefault()
-
-//         $that = $(this);
-
-//         $that.parent().find('li').removeClass('active');
-//         $that.addClass('active');
-//     });
-// })
+list.addEventListener("click", function(e) {
+    if(e.node === list.child && e.target.parentNode.nodeName === 'A') {
+        e.target.parentNode.classList.add('active');
+    };
+});
